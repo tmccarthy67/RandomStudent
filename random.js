83e8267aa7fddf961686e0a7cd2e7675fa029032
tmccarthy67 <deviowa@deviowas-MacBook-Air.local>
2014-06-20 11:57:10 -0500

    added reest and running count

--- random.js
+++ random.js
@@ -1,22 +1,57 @@
 var studentMax;
 var tempStudent;
+var whichStudent;
+
 var students = new Array ();
 
+$(function () {
+	$("#randomButton").click(function() {
+		studentMax = $("#studentNumber").val ();
+		$("#howManyStudents").attr('class', 'hidden');
+		$("#startOver").attr('class', 'visible');
+		randomStudent ();
+	});
+});
 
+$(function () {
+	$("#resetButton").click(function() {
+		studentMax = 0;
+		tempStudent = 0;
+		$("#studentNumber").val ("");
+		students = new Array ();
+		$("#student span").html("");
+		$("#chosen span").html("");
+		$("#howManyStudents").attr('class', 'visible');
+		$("#startOver").attr('class', 'hidden');
+	});
+});
 
-function test()
-  {
-  var userInput = document.getElementById("studentNumber").value;
-  studentMax = userInput;
+function randomStudent () {
+	var whichStudent = (parseInt (Math.random() * studentMax + 1));
+	tempStudent = whichStudent;
+	checkStudent ();
+};
 
-  	tempStudent = parseInt (Math.random() * studentMax + 1);
+function checkStudent () {
+	if (students.length == studentMax)
+	{
+		allChosen();
+		return;
+	}
+	var nextStudent = $.inArray (tempStudent,students);
+	$("#student span").html(tempStudent);
+	$("#chosen span").html(students.join(", "));
+	if (nextStudent === -1) {
+		students.push (tempStudent);
+		$("#student span").html(tempStudent);
+		$("#chosen span").html(students.join(", "));
+	}
+	else {
+		randomStudent();
+	}
+};
 
-  	console.log(tempStudent);
-// add temp student to students array
-// check if next random is part of students
-// report next random and add to students
-//repeat unitl students.length = studentMax
-// add reset button
-
-  }
-
+function allChosen () {
+	console.log ("all chosen");
+	$("#chosen span").html("All students have now gone!");
+}
